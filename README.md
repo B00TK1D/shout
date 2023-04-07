063ab678879ed08f262df3e043bb531abe5a1e8e
B00TK1D <stearns.josiah@gmail.com>
2023-04-07 00:28:59 -0400

    Might work

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# shout
+# shOUT
+
+*Sometimes it feels good to shout into the void*
+
+## Usage:
+
+### Listener
+`python3 listen.py`
+
+### Sender
+`python3 send.py`
+(Then type messages to shout, each line will be sent as a separate message)
+
+OR
+
+`python3 send.py <file>`
+
+This will send the contents of the file as a single message, terminating the connection after sending the message
+(so files can be piped into other files on the listener side)
---
+++ send.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+
+MULTICAST_TTL = 5
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+
+
+if len(sys.argv) > 1:
+    with open(sys.argv[1], 'rb') as f:
+        sock.sendto(f.read(), (MCAST_GRP, MCAST_PORT))
+        sock.sendto(b'', (MCAST_GRP, MCAST_PORT))
+        sys.exit(0)
+
+while True:
+    try:
+        sock.sendto(input().encode() + b'\n', (MCAST_GRP, MCAST_PORT))
+    except:
+        break
